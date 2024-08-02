fc7abd15e35164ac027bcb3186a939f313563292
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-08-02 17:45:55 -0300

    fix: add jakarka annotations

--- src/main/kotlin/com/ohashi/payment_card_service/domain/entities/Amount.kt
+++ src/main/kotlin/com/ohashi/payment_card_service/domain/entities/Amount.kt
@@ -1,8 +1,24 @@
 package com.ohashi.payment_card_service.domain.entities
 
 import com.ohashi.payment_card_service.domain.entities.enums.AmountType
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import java.util.UUID
 
+@Entity
 data class Amount(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    val id: UUID? = null,
+
     val type: AmountType,
-    val value: Double
+    val value: Double,
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    val account: Account
 )
