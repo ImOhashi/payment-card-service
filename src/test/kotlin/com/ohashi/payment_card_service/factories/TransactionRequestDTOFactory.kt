1a4a583d246b883e22fd523e2df255c2ba4944cd
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-08-03 00:00:23 -0300

    fix: add a parameter on method sample signature

--- src/test/kotlin/com/ohashi/payment_card_service/factories/TransactionRequestDTOFactory.kt
+++ src/test/kotlin/com/ohashi/payment_card_service/factories/TransactionRequestDTOFactory.kt
@@ -3,9 +3,9 @@ package com.ohashi.payment_card_service.factories
 import com.ohashi.payment_card_service.application.dto.TransactionRequestDTO
 
 object TransactionRequestDTOFactory {
-    fun sample() = TransactionRequestDTO(
+    fun sample(totalAmount: Double = 100.toDouble()) = TransactionRequestDTO(
         account = "account",
-        totalAmount = 100.toDouble(),
+        totalAmount = totalAmount,
         mcc = "5411",
         merchant = "iFood"
     )
