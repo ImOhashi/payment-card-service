e52779d4afd19055eb85847f758323dcf24cfc7f
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-08-02 22:20:02 -0300

    feat: add amount insert migration

---
+++ src/main/resources/db/migration/V5__insert_amount_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO amount (id, type, value, account_id) VALUES
+    (uuid_generate_v4(), 'FOOD', 100.0, '96f31467-b9fc-448f-98ab-40c4e0789c2d'),
+    (uuid_generate_v4(), 'MEAL', 200.0, '96f31467-b9fc-448f-98ab-40c4e0789c2d'),
+    (uuid_generate_v4(), 'CASH', 150.0, '96f31467-b9fc-448f-98ab-40c4e0789c2d'),
+    (uuid_generate_v4(), 'FOOD', 300.0, '07fe60aa-45be-43f3-8475-afef063f5bbc'),
+    (uuid_generate_v4(), 'MEAL', 250.0, 'd5e963f1-b879-435d-8bca-0b194f592db3'),
+    (uuid_generate_v4(), 'CASH', 500.0, 'd5e963f1-b879-435d-8bca-0b194f592db3');
