bcfc1fd4bbd38bc10641116d4516dc413cfd2df1
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-08-02 22:19:08 -0300

    fix: add return on authorization method

--- src/main/kotlin/com/ohashi/payment_card_service/domain/services/PaymentService.kt
+++ src/main/kotlin/com/ohashi/payment_card_service/domain/services/PaymentService.kt
@@ -1,7 +1,8 @@
 package com.ohashi.payment_card_service.domain.services
 
 import com.ohashi.payment_card_service.application.dto.TransactionRequestDTO
+import com.ohashi.payment_card_service.application.dto.enums.TransactionStatus
 
 interface PaymentService {
-    fun authorization(transactionRequest: TransactionRequestDTO)
+    fun authorization(transactionRequest: TransactionRequestDTO): TransactionStatus
 }
