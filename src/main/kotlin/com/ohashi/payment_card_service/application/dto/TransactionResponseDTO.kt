2d8374bbba34f8491ebd6941e1e6308c576ae891
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-08-02 22:20:51 -0300

    feat: add transaction response dto

---
+++ src/main/kotlin/com/ohashi/payment_card_service/application/dto/TransactionResponseDTO.kt
@@ -0,0 +1,7 @@
+package com.ohashi.payment_card_service.application.dto
+
+import com.ohashi.payment_card_service.application.dto.enums.TransactionStatus
+
+data class TransactionResponseDTO(
+    val code: String
+)
