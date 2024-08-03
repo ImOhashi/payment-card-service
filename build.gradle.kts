cfe721c1568db2dd16e5b319bad45f34c40b382c
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-08-02 22:18:37 -0300

    fix: add log4j2 dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,10 +29,12 @@ dependencies {
 	// Kotlin
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
 
 	// Log4j
 	implementation("org.apache.logging.log4j:log4j-api:2.20.0")
 	implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+	implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
 
 	// Web
 	implementation("org.springframework.boot:spring-boot-starter-web")
